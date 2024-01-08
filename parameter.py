076baf21b72f85546d81d8ba0beb1bc2175ba9eb
LyAn CHHAY <ly-an.chhay@telecom-sudparis.eu>
2024-01-08 03:41:15 +0100

    MAJ

--- parameter.py
+++ parameter.py
@@ -1,20 +1,9 @@
-
-# usecase = "realtime"
-usecase = "historical"
-
-
 crypto_symbols = ["BTC-USD", "ETH-USD", "LTC-USD"]
 
-historical_topics = {
-    "BTC-USD": "historical-BTC-USD-topic",
-    "ETH-USD": "historical-ETH-USD-topic",
-    "LTC-USD": "historical-LTC-USD-topic"
-}
-
-realtime_topics = {
-    "BTC-USD": "realtime-BTC-USD-topic",
-    "ETH-USD": "realtime-ETH-USD-topic",
-    "LTC-USD": "realtime-LTC-USD-topic"
+topics = {
+    "BTC-USD": "BTC-USD-topic",
+    "ETH-USD": "ETH-USD-topic",
+    "LTC-USD": "LTC-USD-topic"
 }
 
 crypto_names = {
@@ -23,12 +12,10 @@ crypto_names = {
     "LTC-USD": "Litecoin"
 }
 
+
 crypto_symbol = "BTC-USD"
 # crypto_symbol =  "ETH-USD"
 # crypto_symbol = "LTC-USD"
 
-historical_topic = historical_topics[crypto_symbol]
-realtime_topic = realtime_topics[crypto_symbol]
+topic = topics[crypto_symbol]
 crypto_name = crypto_names[crypto_symbol]
-
-topic_to_use = historical_topic if usecase == "historical" else realtime_topic
